13af4556649395aed762d12f9daef85795de8a2d
HauDNT <tienhau.it@gmail.com>
2025-11-08 22:30:02 +0700

    Create: Cart icon, favorite button, mobile menu, sign in button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,26 @@
+import CartIcon from "@/components/CartIcon"
 import Container from "@/components/Container"
+import FavoriteButton from "@/components/FavoriteButton"
 import HeaderMenu from "@/components/HeaderMenu"
 import Logo from "@/components/Logo"
+import MobileMenu from "@/components/MobileMenu"
 import SearchBar from "@/components/SearchBar"
+import SignIn from "@/components/SignIn"
 
 const Header = () => {
   return (
     <header className="bg-white py-5 border-b border-b-black/20">
-      <Container className="flex items-center justify-between">
-        <Logo/>
+      <Container className="flex items-center justify-between text-lightColor">
+        <div className="w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0">
+          <MobileMenu/>
+          <Logo/>
+        </div>
         <HeaderMenu/>
-        <div>
+        <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
           <SearchBar/>
+          <CartIcon/>
+          <FavoriteButton/>
+          <SignIn/>
         </div>
       </Container>
     </header>
